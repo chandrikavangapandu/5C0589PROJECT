a7fd4231249732a9eb2e5b387f002be296671187
chand <chand@MSI>
2022-12-09 11:41:42 +0530

    DEVOPS PROJECT

---
+++ src/main/java/C0589PROJECT/C0589PROJECT/App.java
@@ -0,0 +1,52 @@
+package C0589PROJECT.C0589PROJECT;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+	void printApp(int n)
+		{
+			synchronized(this)
+			{
+				for(int i=1;i<=10;i++)
+				{
+					System.out.println(n+"*"+i+"="+(n*i));
+					try
+					{
+						Thread.sleep(1000);
+					}
+					catch(Exception e)
+					{
+						System.out.println(e);
+					}
+				}
+			}		
+		}
+	}
+	class Mythread1 extends Thread
+	{
+		App t;
+		Mythread1(App t)
+		{
+			this.t=t;
+		}
+		public void run()
+		{
+			t.printApp(7);
+		}
+	}
+
+	class thread 
+	{
+		public static void main(String args[])
+		{
+			App obj = new App();
+			Mythread1 th1 = new Mythread1(obj);
+			th1.start();
+			
+		}
+	}
+
